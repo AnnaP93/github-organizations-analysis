50b15c2add75ec1ed4ea6e2d47b4c69460deeda1
AnnaP93 <popovichann56@gmail.com>
2020-12-09 16:08:51 -0500

    Added functionality to check when the record was created and to update it if it was recorded over a week ago. Adjusted to return stats for a single organization

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import requests
 import os
 from dotenv import load_dotenv
 import sys
-from prettytable import PrettyTable
 from sqlite_git_organizations_demo import __fill_organizations_table
-from sqlite_git_organizations_demo import __return_complete_table
+from sqlite_git_organizations_demo import __return_pretty_table
+from sqlite_git_organizations_demo import __return_all_data
+from sqlite_git_organizations_demo import __update_record
+from sqlite_git_organizations_demo import __check_if_company_exist_in_table
 import datetime
+from datetime import timedelta
 
 
 def get_all_repositories_data(organization):
@@ -41,15 +44,32 @@ def __get_api_response(url, company):
     return repositories_in_organization
 
 
+def __check_time_when_added(company):
+    # complete_table = __return_all_data()
+    check_company_existence = __check_if_company_exist_in_table(company)
+    if check_company_existence != []:
+        for record in check_company_existence:
+            date_time_obj = datetime.datetime.strptime(record[5], '%Y-%m-%d %H:%M:%S.%f')
+            if datetime.datetime.now() - date_time_obj >= timedelta(weeks=1):
+                new_stats = get_all_repositories_data(company)
+                updated_tuple = __update_record(tuple(new_stats))
+                return updated_tuple
+            else:
+                return record
+    else:
+        company_stats = get_all_repositories_data(company)
+        inserting_data_into_database = __fill_organizations_table(tuple(company_stats))
+        return inserting_data_into_database
+
+
 if __name__ == "__main__":
     # Credentials
     load_dotenv('token.env')
 
     organizations = sys.argv[1:]
     for single_company in organizations:
-        single_company_stats = get_all_repositories_data(single_company)
-        # inserting_data_into_database = __fill_organizations_table(single_company_stats)
-        complete_table = __return_complete_table()
+        single_company_stats = __check_time_when_added(single_company)
+        return_single_company_stats = __return_pretty_table(single_company)
+        print(return_single_company_stats)
 
-    print(complete_table)
 
